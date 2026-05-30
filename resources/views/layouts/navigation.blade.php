a54434068dfc12dfae1c8e2051ae855216c4aad2
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-05-30 16:27:07 +0400

    fix(views): include tasks and task statuses index pages in mobile navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -78,6 +78,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('task_statuses.index')" :active="request()->routeIs('task_statuses.*')">
+                {{ __('Task Statuses') }}
+            </x-nav-link>
+            <x-responsive-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.*')">
+                {{ __('Tasks') }}
+            </x-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
