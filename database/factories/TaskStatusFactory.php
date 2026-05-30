9ed65785bdaad7fb3decd569df175c7f62520f1e
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-05-31 02:58:58 +0400

    fix(db): correct task status faker from name to word

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -18,7 +18,7 @@ class TaskStatusFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->word(),
         ];
     }
 }
