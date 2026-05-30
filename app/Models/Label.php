6a2f34b3a1c1cbc5cd088078b80235b870b4288f
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-05-30 23:18:23 +0400

    feat(model): add label model

---
+++ app/Models/Label.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['name', 'description'])]
+class Label extends Model
+{
+    use HasFactory;
+
+    public function tasks()
+    {
+        return $this->belongsToMany(Task::class);
+    }
+}
