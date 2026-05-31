22dbf9499b8c773bd861aa7c7ebf429281652646
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-06-01 00:15:36 +0400

    test(label): fix setup deps to include statuses

--- tests/Feature/LabelTest.php
+++ tests/Feature/LabelTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Task;
+use App\Models\TaskStatus;
 use App\Models\Label;
 use App\Models\User;
 
@@ -14,14 +15,17 @@ class LabelTest extends TestCase
 
     private User $user;
     private Label $label;
+    private TaskStatus $status;
     private Task $task;
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->user = User::factory()->create();
+        $this->status = TaskStatus::factory()->create();
         $this->label = Label::factory()->create();
         $this->task = Task::factory()->create([
+            'status_id' => $this->status->id,
             'created_by_id' => $this->user->id,
         ]);
         $this->task->labels()->attach($this->label->id);
